a6c4e6c77a4c27ebaafdc736e921bb5744fc2a5a
Finai24 <redazione@finai24.com>
2025-04-15 15:55:46 +0200

    Update server.js

--- config/server.js
+++ config/server.js
@@ -1,8 +1,6 @@
 module.exports = ({ env }) => ({
-  url: env('RENDER_EXTERNAL_URL'),
-  dirs: {
-    public: './public',
-  },
+  host: '0.0.0.0',
+  port: env.int('PORT', 1337),
   app: {
     keys: env.array('APP_KEYS'),
   },
